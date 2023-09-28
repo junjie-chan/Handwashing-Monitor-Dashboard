71e96fcf9e25ed04706e7ac10c0ad18fb4dfb4d8
Junjie Chen <junjie.chan@outlook.com>
2023-09-28 23:52:56 +1000

    Add login page with css styling.

---
+++ app/Views/errors/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>G'Day</title>
+  <link rel="stylesheet" type="text/css" href="<?= base_url('css/login.css') ?>">
+
+</head>
+<body>
+
+<form>
+  <h1>Access Code</h1>
+  <div class="form-field">
+    <input type="password" placeholder="Enter the code here" required/>   
+  </div>
+  
+  <div class="form-field">
+    <button class="btn" type="submit">Log in</button>
+  </div>
+</form>
+  
+</body>
+</html>
