129ae154eade6d65cd8347f9a3b6725f411800ec
Junjie Chen <junjie.chan@outlook.com>
2023-09-30 13:03:26 +1000

    LAZYCC-56 Restyle canvas size and ensure its height equals to its width with responsive design.

--- app/Views/summary.php
+++ app/Views/summary.php
@@ -9,13 +9,30 @@
 </head>
 
 <body>
-    <div class="container" style="width: 40%;">
+    <div class="container" style="width: 25%; margin-right: 13%;">
         <canvas id="total_beat"></canvas>
     </div>
 
-    <div class="container" style="width: 40%;">
+    <div class="container" style="width: 25%;">
         <canvas id="hourly_rate"></canvas>
     </div>
+
+    <!--  -->
+    <script>
+        // Config the canvas size
+        // Get donuts
+        var donut1 = document.getElementById("total_beat");
+        var donut2 = document.getElementById("hourly_rate");
+        // Get the computed style of donuts
+        var style1 = window.getComputedStyle(donut1);
+        var style2 = window.getComputedStyle(donut2);
+        // Get and modify the width value
+        var width1 = style1.getPropertyValue("width").replace('px', '');
+        var width2 = style1.getPropertyValue("width").replace('px', '');
+        // Reset the height of donuts
+        donut1.height = width1;
+        donut2.height = width2;
+    </script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
     <script src="<?= base_url('javascript/summary.js') ?>"></script>
--- public/javascript/summary.js
+++ public/javascript/summary.js
@@ -2,6 +2,7 @@ const total_beat_y = [55, 45];
 const hourly_rate_y = [55, 25];
 const background_color = ["#b91d47", "white"];
 
+// Create donut charts
 new Chart("total_beat", {
   type: "doughnut",
   data: {
