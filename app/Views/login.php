4f5924e24abc22cfe455047cca05201b1f3ab62a
Junjie Chen <junjie.chan@outlook.com>
2023-09-30 12:12:23 +1000

    Adjust code format login.php.

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<html lang="en" >
+<html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,18 +8,20 @@
   <link rel="stylesheet" type="text/css" href="<?= base_url('css/login.css') ?>">
 
 </head>
+
 <body>
 
-<form>
-  <h1>Access Code</h1>
-  <div class="form-field">
-    <input type="password" placeholder="Enter the code here" required/>   
-  </div>
-  
-  <div class="form-field">
-    <button class="btn" type="submit">Log in</button>
-  </div>
-</form>
-  
+  <form>
+    <h1>Access Code</h1>
+    <div class="form-field">
+      <input type="password" placeholder="Enter the code here" required />
+    </div>
+
+    <div class="form-field">
+      <button class="btn" type="submit">Log in</button>
+    </div>
+  </form>
+
 </body>
+
 </html>
