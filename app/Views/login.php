c9655b8b29a7d1d04d78c1b03b5f6caf8820c92a
Junjie Chen <junjie.chan@outlook.com>
2023-09-30 13:33:01 +1000

    LAZYCC-56 Create a style sheet to store common style settings to reduce code duplication.

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,24 +1,28 @@
 <!DOCTYPE html>
-<html lang="en" >
+<html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>G'Day</title>
+  <link rel="stylesheet" type="text/css" href="<?= base_url('css/common.css') ?>">
   <link rel="stylesheet" type="text/css" href="<?= base_url('css/login.css') ?>">
 
 </head>
+
 <body>
 
-<form>
-  <h1>Access Code</h1>
-  <div class="form-field">
-    <input type="password" placeholder="Enter the code here" required/>   
-  </div>
-  
-  <div class="form-field">
-    <button class="btn" type="submit">Log in</button>
-  </div>
-</form>
-  
+  <form>
+    <h1>Access Code</h1>
+    <div class="form-field">
+      <input type="password" placeholder="Enter the code here" required />
+    </div>
+
+    <div class="form-field">
+      <button class="btn" type="submit">Log in</button>
+    </div>
+  </form>
+
 </body>
+
 </html>
--- app/Views/summary.php
+++ app/Views/summary.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Summary</title>
+    <link rel="stylesheet" type="text/css" href="<?= base_url('css/common.css') ?>">
     <link rel="stylesheet" type="text/css" href="<?= base_url('css/summary.css') ?>">
 </head>
 
