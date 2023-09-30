decd92a05d96f913429e1c8c7a4f8358650dbd4f
Junjie Chen <junjie.chan@outlook.com>
2023-10-01 00:07:24 +1000

    LAZYCC-63 Add labels to show the frequency of handwashing activities in 4 types of time intervals (total, month, week, day).

---
+++ app/Views/dashboard.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Performance Summary</title>
+    <link rel="stylesheet" type="text/css" href="<?= base_url('css/common.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?= base_url('css/dashboard.css') ?>">
+</head>
+
+<body>
+    <h1 style="display: block;">Dashboard of Handwashing Activities</h1>
+    <div id="label_container">
+        <div>
+            <p>Total:</p>
+            <p>869 times</p>
+        </div>
+
+        <div>
+            <p>This Month:</p>
+            <p>386 times</p>
+        </div>
+
+        <div>
+            <p>This Week:</p>
+            <p>105 times</p>
+        </div>
+
+        <div>
+            <p>Today:</p>
+            <p>16 times</p>
+        </div>
+    </div>
+</body>
+
+</html>
