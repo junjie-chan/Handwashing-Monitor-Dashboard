5a24eefe6374b3c072403226c98e30ec2adfedcc
Junjie Chen <junjie.chan@outlook.com>
2023-10-03 23:25:57 +1000

    LAZYCC-67 Set cookies to keep user remian logged in.

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -2,20 +2,40 @@
 
 namespace App\Controllers;
 
+use CodeIgniter\Cookie\Cookie;
+
 class Login extends BaseController
 {
     public function index()
     {
-        return view('login', ['error' => '', 'display' => 'none']);
+        $cookie = json_decode(get_cookie('hospital'));
+        if ($cookie and $cookie->expiry > time()) {
+            return view('dashboard');
+        } else {
+            return view('login', ['error' => '', 'display' => 'none']);
+        }
+        // return view('login', ['error' => '', 'display' => 'none']);
     }
 
     public function check()
     {
         $code = $this->request->getPost('code');
         if ($code == 'deco3801') {
+            // Set Cookie
+            $expiry = time() + 3600 * 8;
+            $data = array(
+                'expiry' => $expiry
+            );
+            setcookie('hospital', json_encode($data), $expiry, '/');
             return view('dashboard');
         } else {
             return view('login', ['error' => 'The access code was incorrect!', 'display' => 'block']);
         }
+        // $code = $this->request->getPost('code');
+        // if ($code == 'deco3801') {
+        //     return view('dashboard');
+        // } else {
+        //     return view('login', ['error' => 'The access code was incorrect!', 'display' => 'block']);
+        // }
     }
 }
